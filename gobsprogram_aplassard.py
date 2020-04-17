6ca4d3fcbf20f1cd72cee5f6ab72a1ef866c0022
Rectambe <rectambe@gmail.com>
2020-04-17 22:11:31 +1000

    changed if statement to use .lower() instead of 'or'

--- gobsprogram_aplassard.py
+++ gobsprogram_aplassard.py
@@ -2,9 +2,9 @@ PENUS = 'PENUS'
 while(True):
     print ("Gob's program: Y/N?")
     s = input("? ")
-    if s == "y" or s == "Y":
+    if s.lower() == "y":
         while(True):
             print("\t".join([PENUS for a in range(6)]))
-    elif s == "n" or s == "N":
+    elif s.lower() == "n":
         break
     print("\a")
