d77e2ad6b648b766ee041fe8747b08b27f88b51f
Jai Wells <rectambe@gmail.com>
2020-05-03 09:36:18 +1000

    Added atributuion comment

--- gobsprogram_aplassard.py
+++ gobsprogram_aplassard.py
@@ -1,3 +1,4 @@
+# Based on Gob's Program by aplassard (Github: https://gist.github.com/aplassard/a368fef46387c16fd19f), Updated to Python 3 by me
 PENUS = 'PENUS'
 while(True):
     print ("Gob's program: Y/N?")
