31e438d42d9fde4383f718536e14526dbc8517b1
Rectambe <rectambe@gmail.com>
2020-08-08 11:10:38 +1000

    Commenting changes

--- gobsprogram.py
+++ gobsprogram.py
@@ -6,15 +6,15 @@ while (True):
     print("Gob's program: Y/N? ")
     ans = input ("? ")
 
-    #Check for a yes or D (for debug) response, then run the program.
+    # Check for a yes or D (for debug) response, then run the program.
     if ans == "Y" or ans == "y" or ans == "D" or ans == "d":
         def penus(num = 60):
             for linenum in range(0, int(num)):
-                #Creates a random delay, rounded to 3 decimal places, to emulate the feel of an old computer
+                # Creates a random delay, rounded to 3 decimal places, to emulate the feel of an old computer
                 sleeptime = round(uniform(0.001, 0.125), 3)
                 print('PENUS    PENUS    PENUS    PENUS    PENUS    PENUS')
                 if ans == "D" or ans == "d":
-                    #Adds the length of the delay and the current line number
+                    # Adds the length of the delay and the current line number
                     print(f"Delay: {sleeptime}, Line: {linenum + 1}")
                 sleep(sleeptime)
 
